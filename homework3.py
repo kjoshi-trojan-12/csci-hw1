a03e841882714c34aba2202f3e7889c235f5c430
Ketan Joshi <kajoshi@usc.edu>
2023-09-18 13:04:58 -0700

    Refactored some methods

--- homework3.py
+++ homework3.py
@@ -1,15 +1,18 @@
 #Imports
+from __future__ import annotations
+import math
 from queue import PriorityQueue
 import queue
 from operator import attrgetter
 import random
 
 
-
-
 #Global Variables
 NUM_CITIES = 0 #Number of cities
 OPTIMAL_FITNESS = 0 #Optimal fitness value (fine-tune this)
+RANK_LIST = [] #List of tuples that contain the index [0] and fitness value [1] of each individual
+POPULATION = [] #List of paths (a permutation of cities)
+MATING_POOL = [] #List of populations selected for mating (list contains paths)
 
 
 
@@ -33,15 +36,34 @@ class Node:
 class Individual:
     def __init__(self, chromosome):
         self.chromosome = chromosome #sequence of genes (objects)
+        self.num_genes = len(chromosome)
         self.fitness = self.calculate_fitness()
 
-    def mutated_genes(self):
-        global GENES
-        gene = random.choice(GENES)
-        return gene
+    def mutate(self):
+        #swap two cities in the chromosome
+        index1 = random.randint(0, self.num_genes - 1)
+        index2 = random.randint(0, self.num_genes - 1)
+        while index1 == index2:
+            index2 = random.randint(0, self.num_genes - 1)
+        temp = self.chromosome[index1]
+        self.chromosome[index1] = self.chromosome[index2]
+        self.chromosome[index2] = temp
+
+    #Fitness function: calculates the fitness of an individual
+    #Parameters: individual (list of cities)
+    #Return: fitness value (float)
+    def calculate_fitness(self):
+        fitness = 0
+        for i in range(self.num_genes - 1):
+            city1 = self.chromosome[i]
+            city2 = self.chromosome[i + 1]
+            distance = city1.calculate_distance(city2)
+            fitness += distance
+
+
 #City data structure is represented as a node (specified above)
 #and also contains coordinates for the location (x, y, z)
-class City(Node, Individual):
+class City(Node):
     def __init__(self, state, parent, action, path_cost, x, y, z):
         super().__init__(state, parent, action, path_cost, heuristic = 0, chromosome = None)
         #state is the name of the city (string)
@@ -51,6 +73,13 @@ class City(Node, Individual):
         self.x = x
         self.y = y
         self.z = z
+
+    #Calculate the distance between two cities
+    #Parameters: city1, city2
+    #Return: distance between city1 and city2
+    def calculate_distance(self, other_city: City):
+        return math.sqrt((self.x - other_city.x)**2 + (self.y - other_city.y)**2 + (self.z - other_city.z)**2)
+        
 
 
 
@@ -88,6 +117,11 @@ class City(Node, Individual):
 #                 setattr(node, 'f', new_g + new_h)
             
 
+#Probablity generator: generates a random number between 0 and 1
+#Return: a random number between 0 and 1
+def probability_generate():
+    return random.random()
+
 #Genetic Search Algorithm
 #Parameters: population, fitness function
 #Return: the best solution found by the algorithm 
@@ -95,25 +129,29 @@ def genetic_search(population: list, fitness_function):
     #repeat until some individual is fit enough or enough time has elapsed in the 
     # population
     while True:
-        #weights
+        weighted_by_population_and_fitness(population, 2)
         population2 = []
         for i in range(len(population)):
             parent1 = None #To-Do: Weighted random selection (population, weights, 2)
             parent2 = None #To-Do: Weighted random selection (population, weights, 2)
-            child = crossover(parent1, parent2) #To-Do: Crossover
-            if True: #To-do: Small random probability
-                child = mutate(child) #To-Do: Mutation
+            child = crossover(parent1, parent2)
+            if(probability_generate() <= 0.01): 
+                child.mutate()
             population2.append(child)
             population = population2
+        if max(population, key = fitness_function).fitness >= OPTIMAL_FITNESS:
+            break
     return max(population, key = fitness_function) #To-Do: Fitness function
 
 
 #Initialize population: initializes the population with a given size and a list of cities
-#Return a list of lists, where each list represents a path of size = size
-def intialize_population(size: int, cities: list):
+#Return a list of paths (a permutation of cities) of size = size
+def initialize_population(size: int, cities: list):
     initial_population = []
     for i in range(size):
-        initial_population.append(cities)
+        path = random.sample(cities, len(cities))
+        individual = Individual(path)
+        initial_population.append(individual)
     return initial_population
 
 
@@ -124,7 +162,10 @@ def intialize_population(size: int, cities: list):
 def parent_selection(population: list, rank_list: list):
     mating_pool = []
     for i in range(len(population)):
-        mating_pool.append(population[rank_list[i]])
+        parent1 = roulette_wheel_selection(population, 1)
+        parent2 = roulette_wheel_selection(population, 1)
+        mating_pool.append(parent1)
+        mating_pool.append(parent2)
     return mating_pool
 
 #Crossover function: creates a child from two parents
@@ -140,9 +181,41 @@ def crossover(parent1: list, parent2: list, start_index: int, end_index: int):
             child.append(parent1[i])
     return child
 
-#Mutation function: mutates a child
-#Parameters: child
+#Weighted random selection: selects a random individual from the population based on their fitness
+#Parameters: population, weights, number of individuals to select
+#Return: list of selected individuals
+def weighted_random_selection(population: list, weights: list, num_individuals: int):
+    selected_individuals = []
+    for i in range(num_individuals):
+        selected_individuals.append(random.choices(population, weights, k = 1))
+    return selected_individuals
 
+#weighted by population and fitness
+#Parameters: population, number of individuals to select
+#Return: list of selected individuals
+def weighted_by_population_and_fitness(population: list, num_individuals: int):
+    selected_individuals = []
+    for i in range(num_individuals):
+        selected_individuals.append(random.choices(population, k = 1))
+    return selected_individuals
+
+#Roullete wheel selection: selects a random individual from the population based on their fitness
+#Parameters: population, number of individuals to select
+#Return: list of selected individuals
+def roulette_wheel_selection(population: list, num_individuals: int):
+    selected_individuals = []
+    total_fitness = 0
+    for individual in population:
+        total_fitness += individual.fitness
+    for i in range(num_individuals):
+        random_num = random.uniform(0, total_fitness)
+        current_sum = 0
+        for individual in population:
+            current_sum += individual.fitness
+            if current_sum > random_num:
+                selected_individuals.append(individual)
+                break
+    return selected_individuals
 
 #Read a file:
 #Parameters: file path
